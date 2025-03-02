2dbf6c736fcd4ebc6bc3085216e06cbc926c731d
Yusuf-GH726 <mmohamedyusuf86@gmail.com>
2025-03-02 09:43:06 +0530

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		float ver = 91.0f; 
+		String dev = "Google" ;
+		boolean isBeta = false;
+		int releaseYear = 2008 ;
+		char shortcutKey = 'C' ;
+		
+		System.out.println("Is this beta version? " + isBeta);
+		System.out.println("The Version of Chrome is " + ver + 
+	                               ", The developer is " + dev + 
+	                               ", Released in the year " + releaseYear + 
+	                               ", Shortcut key: " + shortcutKey);
+	   	            
+	 }
+}
---
+++ day1/LearnDataTypes.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int regno = 726726;
+	boolean isACar = true;
+	float speed = 0.5f;
+	char logo = 'M';
+	String name = "Skoda";
+	long phno = 1234567890;
+	
+	System.out.println("The name of the car is " +name+" The logo is "+logo+" The RegNo is "+regno);
+	System.out.println("The Phone Number is "+phno);	
+	}
+
+}
